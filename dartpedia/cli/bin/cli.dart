f62f5295ece553220db8bf9faaa38529b04cd50b
leopalladium <cileron1305@gmail.com>
2026-03-20 15:38:30 +0100

    Implement game logic and CLI commands for Dartpedia application

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,17 +1,37 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
 const version = '0.0.1'; // Add this line
 
+
 void main(List<String> arguments) {
-  if (arguments.isEmpty || arguments.first == 'help') {
-    printUsage(); // Change this from 'Hello, Dart!'
-  } else if (arguments.first == 'version') {
+  if (arguments.isEmpty || arguments.first == 'help'){
+    printUsage();
+  } else if (arguments.first == 'version'){
     print('Dartpedia CLI version $version');
+  } else if(arguments.first == 'search'){
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
   } else {
-    printUsage(); // Catch-all for any unrecognized command.
+    printUsage();
   }
 }
 
+void searchWikipedia(List<String>? arguments){
+  final String articleTitle;
 
-void printUsage() { // Add this new function
+  if (arguments == null || arguments.isEmpty){
+    print('Please provide an article title.');
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Looking up articles about "$articleTitle". Please wait.');
+  print('Here ya go!');
+  print('(Pretend this is an article about "$articleTitle")');
+}
+
+void printUsage(){
   print(
     "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
   );
