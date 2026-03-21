7d7389e6ee2755b0a3e9d6f6b50489ea5a2ece79
leopalladium <cileron1305@gmail.com>
2026-03-21 01:38:56 +0100

    Implement Wikipedia API search and article retrieval functions; remove unused main function and tests

--- dartpedia/wikipedia/lib/src/api/get_article.dart
+++ dartpedia/wikipedia/lib/src/api/get_article.dart
@@ -1,35 +1,39 @@
-
 import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
 
-import '../model/search_results.dart';
+import '../model/article.dart';
 
-Future<SearchResults> search(String searchTerm) async {
+Future<List<Article>> getArticleByTitle(String title) async {
   final http.Client client = http.Client();
   try {
     final Uri url = Uri.https(
       'en.wikipedia.org',
       '/w/api.php',
       <String, Object?>{
-        'action': 'opensearch',
+        // order matters - explaintext must come after prop
+        'action': 'query',
         'format': 'json',
-        'search': searchTerm,
+        'titles': title.trim(),
+        'prop': 'extracts',
+        'explaintext': '',
       },
     );
     final http.Response response = await client.get(url);
     if (response.statusCode == 200) {
-      final List<Object?> jsonData = jsonDecode(response.body) as List<Object?>;
-      return SearchResults.fromJson(jsonData);
+      final Map<String, Object?> jsonData =
+          jsonDecode(response.body) as Map<String, Object?>;
+      return Article.listFromJson(jsonData);
     } else {
       throw HttpException(
-        '[WikimediaApiClient.getArticleByTitle] '
+        '[ApiClient.getArticleByTitle] '
         'statusCode=${response.statusCode}, '
         'body=${response.body}',
       );
     }
   } on FormatException {
+    // TODO: log
     rethrow;
   } finally {
     client.close();
